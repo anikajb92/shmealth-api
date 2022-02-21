7162474a0544c1a24a91a87bad3d136fa6c7e44a
Anika Bernstein <anikajb92@gmail.com>
2022-02-21 15:21:58 -0700

    changed relationship between prescription and vitals to member

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,5 @@
 class Appointment < ApplicationRecord
   belongs_to :doctor
   belongs_to :member
+  belongs_to :vital
 end
--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -1,3 +1,3 @@
 class Prescription < ApplicationRecord
-  has_and_belongs_to_many :members
+  belongs_to :members
 end
